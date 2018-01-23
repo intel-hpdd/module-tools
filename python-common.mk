9f9af5ae73c0c650c90fa847d0193194a0362f6f
Brian J. Murrell <brian.murrell@intel.com>
2018-01-23 11:05:42 -0500

    Initial code drop
    This bunch of makefile fragments implements the tools needed
    to manage, build and test all of our modules.
    
    Some of the useful make targets are:
    - githooks: Installs the hooks/ subdirectory into your git repo as it
                git hooks
    - rpms: Creates all of the binary RPMs for the module
    - srpm: Creates the source RPM for the module
    - build_test: Performs an RPM build in mock
    - test: Runs any tests of the module
    - tag: Generates any needed files and applies a git tag
    - copr_build: Submits the RPM specfile to Copr for building in a
                  private repo
                  - this requires that a file named copr-local.mk exists
                    in the repo with COPR_OWNER and COPR_PROJECT defined
                    as the owner and project in Copr to build in
    - iml_copr_repo: Submits the RPM specfile to Corp for building in
                     the production IML repo
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

---
+++ python-common.mk
@@ -0,0 +1,12 @@
+test_dependencies:
+	test_deps="$(TEST_DEPS)";                               \
+	if rpm --version && yum --version &&                    \
+	   ! rpm -q $$test_deps >/dev/null 2>&1; then           \
+	    echo "Some dependencies need installing...";        \
+	    echo "You will need sudo root privilledges for yum" \
+	    sudo yum -y install $$test_deps;                    \
+	fi
+
+test: test_dependencies
+	@nosetests $(NOSE_ARGS)
+
